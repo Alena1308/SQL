bab9554af5c492251ceb5f6fdc4565b7f554e783
Alena1308 <Lenamironova13@mail.ru>
2023-03-23 23:59:40 +0300

    Initial commit

---
+++ src/test/java/me/sky/sql/SqlApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sky.sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
